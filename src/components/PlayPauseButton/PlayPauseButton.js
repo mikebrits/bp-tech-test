7bda4656010c508cb71f996a83b83edd9bc46c76
Unknown <mike.brits@gmail.com>
2019-05-24 09:37:20 +0100

    Handle disabled better in the Play Pause button

--- src/components/PlayPauseButton/PlayPauseButton.js
+++ src/components/PlayPauseButton/PlayPauseButton.js
@@ -3,9 +3,12 @@ import { Button, Container } from './PlayPauseButton.styled-components';
 import Icon from '../Icon';
 
 const PlayPauseButton = ({ playing, onClick = () => {}, disabled }) => {
+    const handlePlay = () => {
+        if (!disabled) onClick();
+    };
     return (
         <Container>
-            <Button onClick={onClick} disabled={disabled}>
+            <Button onClick={handlePlay} disabled={disabled}>
                 <Icon size={10} name={playing ? 'pause' : 'play'} />
             </Button>
         </Container>
