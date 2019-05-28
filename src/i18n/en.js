86cf635eb5ec9d785c8ee0c07527a8ab43268a72
Unknown <mike.brits@gmail.com>
2019-05-28 22:43:43 +0100

    i18n translation files

---
+++ src/i18n/en.js
@@ -0,0 +1,26 @@
+export default {
+    key: 'en',
+    header: {
+        search : {
+            placeholder : "Search processes, workers or tags"
+        }
+    },
+    process : {
+        workers : {
+            label : 'Workers Assigned'
+        },
+        status: {
+            Assigned: 'Assigned',
+            Completed: 'Completed',
+            Paused: 'Paused',
+            Unassigned: 'Unassigned'
+        },
+        progressBar : {
+            tasksInQueue: 'Tasks in Queue',
+            tasksCompleted: 'Tasks Completed',
+            averageTaskTime: 'Average Task Time',
+            timeRemaining: 'Time Remaining',
+            seconds: 's',
+        }
+    }
+}
--- src/i18n/jap.js
+++ src/i18n/jap.js
@@ -1,7 +1,26 @@
 export default {
+    key: 'jap',
     header: {
         search : {
-            placeholder : "Search processes, workers or tags"
+            placeholder : "検索プロセス、ワーカー、またはタグ"
+        }
+    },
+    process : {
+        workers : {
+            label : '割り当てられた労働者'
+        },
+        status: {
+            Assigned: '割り当て済み',
+            Completed: '完成した',
+            Paused: '一時停止',
+            Unassigned: '未割り当て\n'
+        },
+        progressBar : {
+            tasksInQueue: 'キュー内のタスク',
+            tasksCompleted: '完了したタスク',
+            averageTaskTime: '平均作業時間',
+            timeRemaining: '残り時間',
+            seconds: '秒',
         }
     }
 }
